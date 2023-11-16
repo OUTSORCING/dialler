a9bee4c2431591ae852b709eb0ca1f31479b13e8
OUTSORCING <operator14@center-personal.info>
2023-11-16 14:24:19 +0000

    404 error

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -7,7 +7,7 @@
       minimumHeaders: 3,
       headers: 'h1, h2, h5, h6',
       listType: 'ol', // values: [ol|ul]
-      showEffect: 'show', // values: [show|slideDown|fadeIn|none]
+      showEffect: 'fadeIn', // values: [show|slideDown|fadeIn|none]
       showSpeed: 'slow', // set to 0 to deactivate effect
       classes: { list: '',
                  item: '',
